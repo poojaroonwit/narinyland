5e605bcc890d6b375644aac6c9dfc2a69551028c
poojaroonwit <jaroonwit.pool@gmail.com>
2026-02-14 02:21:48 +0700

    debug

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
         proxy: {
           '/api': {
-            target: 'http://localhost:4000',
+            target: 'https://narinyland-server.vercel.app',
             changeOrigin: true,
           },
         },
