af510a04a27c0b69263ce5593e338103fe858417
Jaroonwit poolnai <jaroonwit.pool@gmail.com>
2026-08-24 11:50:51 +0700

    style: remove unused Google font preconnects

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,8 +46,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
       <body>
